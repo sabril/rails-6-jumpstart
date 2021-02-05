c8af5dabc90c0b147a05b04604e8777c12fb2f67
Syaiful <syaiful.sabril@gmail.com>
2021-02-05 21:43:29 +0700

    change from pg to sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2021_02_04_045028) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
